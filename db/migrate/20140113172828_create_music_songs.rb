0c457f370f48f6d15ce6f74bf09cd737d08bc30f
Thai Le <lnthai2002@gmail.com>
2014-01-13 14:37:22 -0500

    music: expose migration from this engine to main app. Migration taken from main app

---
+++ db/migrate/20140113172828_create_music_songs.rb
@@ -0,0 +1,14 @@
+class CreateMusicSongs < ActiveRecord::Migration
+  def change
+    create_table :music_songs do |t|
+      t.column :title, :string
+      t.column :artist, :string
+      t.column :album, :string
+      t.column :tracknum, :integer
+      t.column :genre, :string, :limit=>50
+      t.column :comments, :string
+      t.column :location, :string
+      t.timestamps
+    end
+  end
+end
--- lib/music/engine.rb
+++ lib/music/engine.rb
@@ -1,5 +1,15 @@
 module Music
   class Engine < ::Rails::Engine
     isolate_namespace Music
+
+    #to avoid copy engine migration to main app migration, just tell the main app that there is migration in this engine
+    #see: http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
