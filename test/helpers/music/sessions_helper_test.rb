b2c34e2724ef07def99cad3343155707acada759
Thai Le <lnthai2002@gmail.com>
2014-01-13 14:45:43 -0500

    music: test suit

---
+++ test/helpers/music/sessions_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Music
+  class SessionsHelperTest < ActionView::TestCase
+  end
+end
---
+++ test/helpers/music/songs_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Music
+  class SongsHelperTest < ActionView::TestCase
+  end
+end
