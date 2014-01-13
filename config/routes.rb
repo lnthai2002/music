c6d8e558def34f8c144a81fa51f6a0b4d32fc69e
Thai Le <lnthai2002@gmail.com>
2014-01-13 14:45:22 -0500

    music: song resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Music::Engine.routes.draw do
+  resources :songs
+#  match 'songs/download/:id'=>'songs#download', :id => /[^\/]+/
+
+  delete 'logout', to: 'sessions#logout'
+
+  root :to => 'songs#index'
 end
